375bcb94abf5cbdac1c8fdf276b71766ce80203f
Stepa <kovtunstepa3@gmail.com>
2021-09-20 15:18:26 +0300

    1234

---
+++ .vscode/20,09.py
@@ -0,0 +1,11 @@
+"""
+student_1 = {'group': '9.2.6',
+                'age': 18}
+student_2 =  {'group': '9.2.1',
+                'age': 19}
+students =  {'Vasya' : student_1,
+                'Ivan': student_2}
+print(students)
+
+"""
+
