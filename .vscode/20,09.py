49d60813a05f15a24756ee2406f92794c2a3ddc1
Stepa <kovtunstepa3@gmail.com>
2021-09-26 17:57:23 +0300

    123456

--- .vscode/20,09.py
+++ .vscode/20,09.py
@@ -1,11 +1,4 @@
-"""
-student_1 = {'group': '9.2.6',
-                'age': 18}
-student_2 =  {'group': '9.2.1',
-                'age': 19}
-students =  {'Vasya' : student_1,
-                'Ivan': student_2}
-print(students)
-
-"""
-
+a = input()
+b = input()
+s = a + b
+print(s)
