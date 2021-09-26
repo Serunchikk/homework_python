7ce06290e1b8dacc00697b694255b8fd12f2d1d9
Stepa <kovtunstepa3@gmail.com>
2021-09-26 18:16:30 +0300

    123456

--- homework/Генератор10,09.py
+++ homework/Генератор10,09.py
@@ -1,40 +1,10 @@
-import string 
-def F(n):
-    for i in range(len(n)):
-        print(n[i])
-
-def F2(n):
-    a = len(n)
-    d = 0 
-    while d < a:
-        print(n[d])
-        d += 1
+def gen_names(name, num):
+    i = 1
+    while i < num:
+        yield name
+        i += 1
 
 
-
-
-
-print(list(string.ascii_lowercase))
-
-ders = [chr(i) for i in range(97,123)]
-
-'''for i in range(97,123):
-    a = chr(i)
-    ders.append(a)
-print(spisok)'''
-
-
-
-i = 97
-ders1 = []
-while i < 123:
-    
-    a = chr(i)
-    ders1.append(a)
-    i += 1
-print(ders1)
-print('1')
-
-F(ders)
-print()
-F2(ders)
+ranger = gen_names('Stepa', 50)
+names = [x for x in ranger]
+print(names)
