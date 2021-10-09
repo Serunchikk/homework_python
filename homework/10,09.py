7a28843413f8a18a305835446e8a529a54c21a00
Stepa <kovtunstepa3@gmail.com>
2021-10-09 12:21:36 +0300

    сделал оставшиеся дз

--- homework/10,09.py
+++ homework/10,09.py
@@ -5,4 +5,17 @@ def show(func):
         print('Keyword arguments are: ', kwargs)
         
     return new_func
- 
+@show
+def initials_PRO(names):
+    b = len(names)
+    new_names = []
+    for i in range(b):
+        a,b,c= names[i].split(' ')
+        a +=' ' + b[0]+ ' ' + c[0]
+        new_names.append(a)
+    return new_names
+
+
+spisok=['Ковтун Степан Ильич ','Меняйло Екатерина Андреевна','Бухтиничева Ксения Геннадьвна','Яцына Леонид Сергеевич','Бабков Никита Алексеевич']
+
+print(initials_PRO(spisok))
